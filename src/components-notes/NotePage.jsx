29ca1ae03905eb693f30e0dbf7c04c6622f5f000
shubham <shubhamg20@iitk.ac.in>
2021-08-03 02:51:26 +0530

    Pending:on reload notes get dissapear

---
+++ src/components-notes/NotePage.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+function NotePage(props) {
+  var info = {};
+  // info={action:"get"}
+  // fetch("http://localhost:9002/notes", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //   body: JSON.stringify(info),
+  // }).then((res) => res.json()).then((res)=>{
+  //   console.log(res);
+  //    setNotes(res);
+  // })
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    info = { note, action: "add" };
+    console.log(info, note);
+    fetch("http://localhost:9002/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: JSON.stringify(info),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setNotes(res);
+      });
+  }
+
+  function deleteNote(note) {
+    info = { note, action: "delete" };
+    fetch("http://localhost:9002/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: JSON.stringify(info),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setNotes(res);
+      });
+  }
+
+  return (
+    <div>
+      <Header setUser={props.setUser} />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default NotePage;
